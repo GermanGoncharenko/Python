e17354f3c025946fd78d3d8c0eecb8c435aafcc0
GermanGoncharenko <gonharenkogerman@gmail.com>
2021-03-01 17:57:34 +0300

    ДЗ к 4

--- Task_4_4.py
+++ Task_4_4.py
@@ -1,15 +1,18 @@
 from requests import get, utils
 response = utils.get_unicode_from_response(get('http://www.cbr.ru/scripts/XML_daily.asp'))
-
 def currency_rates(code):
     content = response.split("<Valute ID=")
     for i in content:
         if code.upper() in i:
             return float(i.replace("/", "").split("<Value>")[-2].replace(",", "."))
 
+if __name__ == "__main__":
+    print(currency_rates("USD"))
+    print(currency_rates("EUR"))
 
-print(currency_rates("USD"))
-print(currency_rates("EUR"))
+#-------------------------------------
 
+import Task_4_4
+print(Task_4_4.currency_rates("CRY"))
 
 

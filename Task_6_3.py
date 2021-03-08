c4cc74d431f76db3feceb768b59b744bd6cef0ce
GermanGoncharenko <78817510+GermanGoncharenko@users.noreply.github.com>
2021-03-08 20:10:27 +0300

    Update Task_6_3.py
    После исправлений

--- Task_6_3.py
+++ Task_6_3.py
@@ -1,3 +1,12 @@
 from json import dump
 from itertools import zip_longest
-with open ("users.csv", "r", encoding="utf-8") as users:
+with open ("name.csv", "r", encoding="utf-8") as name:
+    with open("hobby.csv", "r", encoding="utf-8") as hobby:
+        box = zip_longest((" ".join(us.split(",")) for us in name), hobby, fillvalue=None)
+        my_box = {str(el[0]).strip(): (el[1].strip()) for el in box}
+        with open('dict_n_h.json', 'w', encoding='utf-8') as f:
+            if "None" in my_box:
+                print(1)
+            else:
+                dump(my_box, f, ensure_ascii=False, indent=4)
+                print(my_box)

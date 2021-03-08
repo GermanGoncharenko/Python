9b83bee47e19634624a694f6f7c5c8e836c66687
GermanGoncharenko <gonharenkogerman@gmail.com>
2021-03-08 19:56:12 +0300

    Lesson_6

--- Task_5_2.py
+++
@@ -1,8 +0,0 @@
-gen = (num for num in range(1, 23 + 1, 2))
-# print(*gen) # Можно вывести в одну строку с пробелом
-# print(*gen, sep='') # вывести в одну строку слитно
-# print(next(gen), next(gen)) # последовательно нужное количество(в строку)
-print(next(gen)) # по очереди(в столбик) друг за другом прописывая print
-print(next(gen))
-print(next(gen))
-print(next(gen))
--- Task_5_3.py
+++
@@ -1,7 +0,0 @@
-from itertools import islice, zip_longest
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
-klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А']
-result = (i for i in zip_longest(tutors, klasses))
-print(*islice(result, 7))      # Результат
-print(type(result))             # Тип класса
-print(list(islice(result, 9)))  # Работа до истощения

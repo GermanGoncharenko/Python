0b5bb922c67157b4d9ed6e5e782a54481710fa9b
GermanGoncharenko <gonharenkogerman@gmail.com>
2021-03-18 18:48:05 +0300

    DZ_8

---
+++ Task_8_1.py
@@ -0,0 +1,15 @@
+from re import compile
+
+def email_parse(email_address):
+    name = compile(r'([\w]+)\@((?<==@)[^.]+\.\w+)')
+    if name.match(email_address):
+        a, b = name.findall(email_address)[0]
+        print(dict(name=a, domain=b))
+    else:
+        raise ValueError
+
+try:
+    email_parse('someone@geekbrains.ru')
+    email_parse('someone@geekbrainsru')
+except ValueError:
+    print(f'Wrong email!')
